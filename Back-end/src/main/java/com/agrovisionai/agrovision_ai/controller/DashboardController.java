2c2f3202c24a5c8417482529a97e55baa576f10f
João Pedro <bfjoaop@gmail.com>
2026-08-12 11:54:13 -0300

    Controller basica de dashboard

--- Back-end/src/main/java/com/agrovisionai/agrovision_ai/controller/DashboardController.java
+++ Back-end/src/main/java/com/agrovisionai/agrovision_ai/controller/DashboardController.java
@@ -1,6 +1,10 @@
 package com.agrovisionai.agrovision_ai.controller;
 
+import com.agrovisionai.agrovision_ai.domain.dto.response.DashboardResponseDTO;
+import com.agrovisionai.agrovision_ai.service.DashboardService;
 import org.aspectj.lang.annotation.DeclareError;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/dashboard")
 public class DashboardController {
+    @Autowired
+    DashboardService dashboardService;
 
     @GetMapping
-    @PreAuthorize("hasRole('PRODUTOR') or hasRole('FUNCIONARIO')")
-    public
+    @PreAuthorize("hasRole('PRODUTOR')")
+    public ResponseEntity<DashboardResponseDTO> getDashboard() {
+        DashboardResponseDTO dashboard = dashboardService.criarDashboard();
+        return ResponseEntity.ok(dashboard);
+    }
 }
